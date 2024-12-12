77324009f6896bebcb077f5eaf3abcdaf4846d03
creator928 <creator928forgit@gmail.com>
2024-12-12 15:26:21 +0900

    retrieval and run_retrieval updated.

--- chatbot/retrieval/retrieval.py
+++ chatbot/retrieval/retrieval.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 
 class Retrieval:
     def __init__(self, data_path: str = "./data/newjeans.json"):
@@ -9,20 +9,12 @@ class Retrieval:
 
     def retrieve(self, query: str) -> str | None:
         # ####
-        # 이 주석을 지우고 다음 코드를 완성해보자.
-        # query 는 유저의 발화문이다. self.data 에는 json 값이 key-value 형태로 저장되어 있다.
-        # 만약 query 가 json 의 key 값을 키워드로 포함하고 있으면 "{key}: {value}" 를 str 으로 리턴하자.
-        # 아니라면 None 을 리턴하자.
-        #
-        # 예시 1:
-        # query: who is minji? ("minji" is in json key)
-        # @ return "minji: The leader of NewJeans..."
-        # 
-        #
-        # 예시 2:
-        # query: this is an empty query (None of the json key matches)
-        # @ return None
+        for key, value in self.data.items():
+            if key.lower() in query.lower():  # 대소문자 구분 없이 검색
+                return f"{key}: {value}"
+        return None
         # ###
+
         pass
 
     def print_data(self) -> None:
